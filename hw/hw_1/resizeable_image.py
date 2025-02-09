8dc9d0b67c8e449d9a9b7285191fc84490b70941
gordonbchen <gordonbchen@gmail.com>
2025-02-09 13:21:24 -0500

    format hw 1

--- hw/hw_1/resizeable_image.py
+++ hw/hw_1/resizeable_image.py
@@ -1,9 +1,12 @@
-from imagematrix import ImageMatrix
-import numpy as np
 import time
+import numpy as np
 
-class ResizeableImage(ImageMatrix):    
+from imagematrix import ImageMatrix
+
+
+class ResizeableImage(ImageMatrix):
     def best_seam(self, dp: bool = True) -> list[tuple[int, int]]:
+        """Compute the lowest energy vertical seam."""
         if dp:
             return self._best_seam_dp()
         return self._best_seam_recur()
@@ -12,7 +15,7 @@ class ResizeableImage(ImageMatrix):
         """Compute the best seam using dynamic programming."""
         # Assumes non-neg int energy. Use a dict if this is not true.
         min_energy_table = np.full((self.width, self.height), -1, dtype=np.int32)
-        min_ind_table = np.empty_like(min_energy_table)
+        min_ind_table = min_energy_table.copy()
         energy_cache = min_energy_table.copy()
 
         # Base case.
@@ -22,9 +25,11 @@ class ResizeableImage(ImageMatrix):
         # Fill in the rest of the table using the recursive relation.
         for j in range(1, self.height):
             for i in range(self.width):
-                if min_energy_table[i,j] == -1:
-                    self._calc_min_energy_dp(i, j, min_energy_table, min_ind_table, energy_cache)
-        
+                if min_energy_table[i, j] == -1:
+                    self._calc_min_energy_dp(
+                        i, j, min_energy_table, min_ind_table, energy_cache
+                    )
+
         min_i = min_energy_table[:, -1].argmin()
 
         # Backtrack.
@@ -32,25 +37,36 @@ class ResizeableImage(ImageMatrix):
         for j in reversed(range(self.height - 1)):
             seam.append((min_ind_table[seam[-1]], j))
         return seam
-    
+
     def _get_cached_energy(self, i: int, j: int, energy_cache: np.ndarray) -> int:
         """Returns the cached or calculated energy of the pixel [i,j]."""
         if energy_cache[i, j] == -1:
-            energy_cache[i,j] = self.energy(i, j)
-        return energy_cache[i,j]
+            energy_cache[i, j] = self.energy(i, j)
+        return energy_cache[i, j]
 
-    def _calc_min_energy_dp(self, i: int, j: int, min_energy_table: np.ndarray, min_ind_table: np.ndarray, energy_cache: np.ndarray) -> None:
-        """Calculate the min energy and the previous column of a vertical seam ending at pixel [i, j] using DP."""
+    def _calc_min_energy_dp(
+        self,
+        i: int,
+        j: int,
+        min_energy_table: np.ndarray,
+        min_ind_table: np.ndarray,
+        energy_cache: np.ndarray,
+    ) -> None:
+        """Compute the min energy of a vertical seam ending at pixel [i, j] using DP."""
         prev_j = j - 1
         left_i, right_i = max(0, i - 1), min(i + 2, self.width)
-        min_prev_i = min(range(left_i, right_i), key=lambda idx: min_energy_table[idx, prev_j])
+        min_prev_i = min(
+            range(left_i, right_i), key=lambda idx: min_energy_table[idx, prev_j]
+        )
 
-        min_energy_table[i, j] = min_energy_table[min_prev_i, prev_j] + self._get_cached_energy(i, j, energy_cache)
+        min_energy_table[i, j] = min_energy_table[
+            min_prev_i, prev_j
+        ] + self._get_cached_energy(i, j, energy_cache)
         min_ind_table[i, j] = min_prev_i
-    
+
     def _best_seam_recur(self) -> list[tuple[int, int]]:
         """Compute the best seam using naive recursion."""
-        energy_cache = np.full((self.width, self.height), -1, dtype=np.int32) 
+        energy_cache = np.full((self.width, self.height), -1, dtype=np.int32)
         min_energy = float("inf")
         best_seam = None
         for i in range(self.width):
@@ -59,22 +75,25 @@ class ResizeableImage(ImageMatrix):
                 min_energy = energy
                 best_seam = seam
         return best_seam
-    
-    def _calc_min_energy_recur(self, i: int, j: int, energy_cache: np.ndarray) -> tuple[int, list[tuple[int, int]]]:
-        """Calculate the min energy and the path of the best vertical seam ending at pixel [i,j] recursively."""
+
+    def _calc_min_energy_recur(
+        self, i: int, j: int, energy_cache: np.ndarray
+    ) -> tuple[int, list[tuple[int, int]]]:
+        """Compute the min energy of the best vertical seam ending at pixel [i,j] recursively."""
         if j == 0:
-            return self._get_cached_energy(i, j, energy_cache), [(i,j)]
+            return self._get_cached_energy(i, j, energy_cache), [(i, j)]
 
         min_energy = float("inf")
         best_seam = None
-        for prev_i in range(max(0, i-1), min(i+2, self.width)):
-            energy, seam = self._calc_min_energy_recur(prev_i, j-1, energy_cache)
+        for prev_i in range(max(0, i - 1), min(i + 2, self.width)):
+            energy, seam = self._calc_min_energy_recur(prev_i, j - 1, energy_cache)
             if energy < min_energy:
                 min_energy = energy
                 best_seam = seam
         min_energy += self._get_cached_energy(i, j, energy_cache)
-        return min_energy, [(i,j)] + best_seam 
-        
+        return min_energy, [(i, j)] + best_seam
+
+
 if __name__ == "__main__":
     img = ResizeableImage("sunset_full.png")
 
--- hw/hw_1/time_viz.py
+++ hw/hw_1/time_viz.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from resizeable_image import ResizeableImage
 
+
 def create_random_image(size: int) -> ResizeableImage:
     pixels = np.random.randint(0, 256, size=(size, size, 3)).astype(np.uint8)
     image = ResizeableImage(Image.fromarray(pixels))
     return image
 
-def time_seams(images: list[ResizeableImage], dp: bool) -> tuple[list[int], list[float]]:
+
+def time_seams(
+    images: list[ResizeableImage], dp: bool
+) -> tuple[list[int], list[float]]:
     sizes = []
     times = []
     print(f"{dp=}")
@@ -22,11 +26,17 @@ def time_seams(images: list[ResizeableImage], dp: bool) -> tuple[list[int], list
         print(f"{str(image.height):7s} {elapsed_time:.6f} sec")
     return sizes, times
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     small_images = [create_random_image(s) for s in range(1, 14)]
-    large_images = [create_random_image(100*s) for s in range(1, 13)]
-    given_images = list(map(ResizeableImage, ("sunset_small.png", "sunset_full.png", "cat_fortress_small.jpg")))
-    
+    large_images = [create_random_image(100 * s) for s in range(1, 13)]
+    given_images = list(
+        map(
+            ResizeableImage,
+            ("sunset_small.png", "sunset_full.png", "cat_fortress_small.jpg"),
+        )
+    )
+
     plt.plot(*time_seams(given_images, dp=True), "r.-", label="DP Given")
     plt.plot(*time_seams(small_images + large_images, dp=True), "g.-", label="DP")
     plt.plot(*time_seams(small_images, dp=False), "b.-", label="Recur")
@@ -36,4 +46,3 @@ if __name__ == '__main__':
     plt.ylabel("Time (sec)")
     plt.legend(loc="best")
     plt.savefig("seam_times.png")
-
