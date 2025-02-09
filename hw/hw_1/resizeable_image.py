6873ab48ce97b0d55fe4705da4f49b1771aec921
gordonbchen <gordonbchen@gmail.com>
2025-02-08 21:03:54 -0500

    use table instead of cache for best seam dp

--- hw/hw_1/resizeable_image.py
+++ hw/hw_1/resizeable_image.py
@@ -1,51 +1,63 @@
 from imagematrix import ImageMatrix
+import numpy as np
+import time
 
-class ResizeableImage(ImageMatrix):
+class ResizeableImage(ImageMatrix):    
     def best_seam(self, dp: bool = True) -> list[tuple[int, int]]:
-        # TODO: support dp = False.
-        min_energy_cache = {}
-        energy_cache = {}
+        if dp:
+            return self._best_seam_dp()
+        return self._best_seam_recur()
 
-        min_energy = None
-        min_i = None
+    def _best_seam_dp(self) -> list[tuple[int, int]]:
+        """Compute the best seam using dynamic programming."""
+        # Assumes non-neg int energy. Use a dict if this is not true.
+        min_energy_table = np.full((self.width, self.height), -1, dtype=np.int32)
+        min_ind_table = np.empty_like(min_energy_table)
+        energy_cache = min_energy_table.copy()
+
+        # Base case.
         for i in range(self.width):
-            energy, _ = self._calc_min_energy(i, self.height-1, min_energy_cache, energy_cache)
-            if (min_energy is None) or (energy < min_energy):
-                min_energy = energy
-                min_i = i
+            min_energy_table[i, 0] = self._get_cached_energy(i, 0, energy_cache)
+
+        # Fill in the rest of the table using the recursive relation.
+        for j in range(1, self.height):
+            for i in range(self.width):
+                if min_energy_table[i,j] == -1:
+                    self._calc_min_energy(i, j, min_energy_table, min_ind_table, energy_cache)
+        
+        min_i = min_energy_table[:, -1].argmin()
 
         # Backtrack.
         seam = [(min_i, self.height - 1)]
         for j in reversed(range(self.height - 1)):
-            seam.append((min_energy_cache[seam[-1]][1], j))
+            seam.append((min_ind_table[*seam[-1]], j))
         return seam
+    
+    def _get_cached_energy(self, i: int, j: int, energy_cache: np.ndarray) -> int:
+        """Returns the cached or calculated energy of the pixel [i,j]."""
+        if energy_cache[i, j] == -1:
+            energy_cache[i,j] = self.energy(i, j)
+        return energy_cache[i,j]
 
-    def _calc_min_energy(self, i: int, j: int, cache: dict, energy_cache: dict) -> tuple[int, int]:
-        """
-        Calculate the min energy and the previous column 
-        of a vertical seam ending at pixel [i,j].
-        """        
-        if (i,j) in cache:
-            return cache[(i,j)]
-
-        min_prev_energy = None
-        min_prev_i = None
+    def _calc_min_energy(self, i: int, j: int, min_energy_table: np.ndarray, min_ind_table: np.ndarray, energy_cache: np.ndarray) -> None:
+        """Calculate the min energy and the previous column of a vertical seam ending at pixel [i, j]."""
         prev_j = j - 1
-        for prev_i in range(i - 1, i + 2):
-            if (prev_i in range(self.width)) and (prev_j in range(self.height)):
-                prev_energy = self._calc_min_energy(prev_i, prev_j, cache, energy_cache)[0]
-                if (min_prev_energy is None) or (prev_energy < min_prev_energy):
-                    min_prev_energy = prev_energy
-                    min_prev_i = prev_i
-                    
+        left_i, right_i = max(0, i - 1), min(i + 2, self.width)
+        min_prev_i = min(range(left_i, right_i), key=lambda idx: min_energy_table[idx, prev_j])
 
-        if (i, j) not in energy_cache:
-            energy_cache[(i,j)] = self.energy(i, j)
+        min_energy_table[i, j] = min_energy_table[min_prev_i, prev_j] + self._get_cached_energy(i, j, energy_cache)
+        min_ind_table[i, j] = min_prev_i
+    
+    def _best_seam_recur(self) -> list[tuple[int, int]]:
+        """Compute the best seam using naive recursion."""
+        # TODO: Implement recursive best seam.
+        pass
 
-        min_energy = (min_prev_energy or 0) + energy_cache[(i,j)]
-        cache[(i,j)] = (min_energy, min_prev_i)
-        return cache[(i,j)]
 
 if __name__ == "__main__":
-    img = ResizeableImage("sunset_small.png")
-    print(img.best_seam())
+    img = ResizeableImage("sunset_full.png")
+
+    t0 = time.time()
+    for i in range(100):
+        img.best_seam()
+    print(time.time() - t0)
